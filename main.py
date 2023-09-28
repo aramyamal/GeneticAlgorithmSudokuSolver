77609303d02e851b73695625b3548db01a6ac266
Aram Jamal <aramyamal@gmail.com>
2023-09-28 22:25:57 +0200

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import GeneticAlgorithmSudokuSolver as ga
 import sudoku_examples
 
-ga.sudokuGA(sudoku_examples.easy, show_continous_plot=True, show_final_plot=True, print_final_board=True)
+ga.sudokuGA(sudoku_examples.hard, show_continous_plot=True, show_final_plot=True, print_final_board=True)
